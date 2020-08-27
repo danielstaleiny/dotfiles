07b96489e3f9900898a89c71ad563dd1f462e72d
danielstaleiny <daniel.rafaj@tuta.io>
2020-08-27 17:29:09 +0200

    home manager settings

--- .nixos/configuration.nix
+++ .nixos/configuration.nix
@@ -1,18 +1,174 @@
 # 
 { config, lib, pkgs, ... }:
 let
-  url = "https://github.com/colemickens/nixpkgs-wayland/archive/master.tar.gz";
-  waylandOverlay = (import (builtins.fetchTarball url));
+  # url = "https://github.com/colemickens/nixpkgs-wayland/archive/master.tar.gz";
+  # waylandOverlay = (import (builtins.fetchTarball url));
   schema = pkgs.gsettings-desktop-schemas;
   datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+  home-manager = (builtins.fetchGit {
+    url = "https://github.com/rycee/home-manager.git";
+    rev = "5c639ff68abf0f16f2e63fb3f8aca016d54c7d10"; 
+    ref = "master";
+  });
+  cachixpkgs = (import (builtins.fetchTarball { url = "https://cachix.org/api/v1/install"; }) {});
 in
 {
   imports =
     [
       ./hardware-configuration.nix
+      (import "${home-manager}/nixos")
     ];
 
-  nixpkgs.overlays = [ waylandOverlay];
+  home-manager.users.anon = {pkgs, ...}: {
+    nixpkgs.config.allowUnfree = true;
+    home.packages = []
+      ++ (with cachixpkgs; [ cachix ])
+      ++ (with pkgs; [
+      htop
+      gotop
+      fzf
+      unzip
+      autojump
+      ncdu
+      zathura
+      zeal
+      wine
+      playonlinux
+	    nix-prefetch-git
+      pmount
+      ripgrep
+      ripgrep-all
+      bat # like cat but with syntax
+      zim
+      whois
+      youtube-dl
+      gimp
+      imagemagick
+
+      lf # like ranger
+      tldr # man examples
+	    # cage
+      htop
+	    vim
+	    emacs
+      chromium
+      lollypop
+      alacritty #Terminal
+      rofi #X11 Launcher
+      #PROGRAMMING
+      git
+      nodejs
+      python
+      dhall
+      docker-compose
+      pandoc
+      texlive.combined.scheme-full
+      tectonic
+      plantuml
+      gdb
+      jq
+      ispell
+      filezilla
+      vlc
+      evince # pdfviewer
+      inkscape
+      libreoffice
+      gimp
+      krita
+      blender
+      tdesktop # Telegram desktop client
+      gnumake
+      #GTK SETTINGS
+      lxappearance
+      gtk_engines
+      glib
+      pango  # fonts stuff
+      arc-theme
+      arc-icon-theme
+      adementary-theme
+      gnome3.adwaita-icon-theme
+      adwaita-qt
+      # remove if they are not doing anything
+      atk
+      at-spi2-atk
+      xdg_utils
+      steam
+      steam-run
+      gnome3.nautilus #file manager
+      gnome3.eog
+    ]);
+
+    wayland.windowManager.sway = {
+      enable = true;
+      config = null;
+      extraConfig = builtins.readFile "/home/anon/.config/sway/.config";
+    };
+    programs.qutebrowser = {
+      enable = true;
+      extraConfig = ''
+        c.auto_save.session = True
+        '';
+    };
+    programs.firefox = {
+      enable = true;
+    #   profiles.anon.settings = {
+    #       "beacon.enabled" = false;
+    #       # "browser.display.background_color" = "#c5c8c6";
+    #       # "browser.display.foreground_color" = "#1d1f21";
+    #       "browser.safebrowsing.appRepURL" = "";
+    #       "browser.safebrowsing.malware.enabled" = false;
+    #       "browser.search.hiddenOneOffs" = "Google,Yahoo,Bing,Amazon.com,Twitter";
+    #       "browser.search.suggest.enabled" = false;
+    #       "browser.send_pings" = false;
+    #       "browser.startup.page" = 3;
+    #       "browser.tabs.closeWindowWithLastTab" = false;
+    #       "browser.urlbar.placeholderName" = "DuckDuckGo";
+    #       "browser.urlbar.speculativeConnect.enabled" = false;
+    #       # "devtools.theme" = "${config.theme.base16.kind}";
+    #       "dom.battery.enabled" = false;
+    #       "dom.event.clipboardevents.enabled" = false;
+    #       "experiments.activeExperiment" = false;
+    #       "experiments.enabled" = false;
+    #       "experiments.supported" = false;
+    #       "extensions.pocket.enabled" = false;
+    #       "general.smoothScroll" = false;
+    #       "geo.enabled" = false;
+    #       "layout.css.devPixelsPerPx" = "1";
+    #       "media.navigator.enabled" = false;
+    #       "media.video_stats.enabled" = false;
+    #       "network.IDN_show_punycode" = true;
+    #       "network.allow-experiments" = false;
+    #       "network.dns.disablePrefetch" = true;
+    #       "network.http.referer.XOriginPolicy" = 2;
+    #       "network.http.referer.XOriginTrimmingPolicy" = 2;
+    #       "network.http.referer.trimmingPolicy" = 2;
+    #       "network.prefetch-next" = false;
+    #       "permissions.default.shortcuts" = 2; # Don't steal my shortcuts!
+    #       "privacy.donottrackheader.enabled" = true;
+    #       "privacy.donottrackheader.value" = 1;
+    #       "privacy.firstparty.isolate" = true;
+    #       "signon.rememberSignons" = false;
+    #       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    #       "default.zoom" = "133%";
+    #       # "widget.content.gtk-theme-override" = "Adwaita:light";
+
+    #   };
+    };
+
+
+    gtk = {
+      enable = true;
+      font.name = "Noto Sans Mono 16";
+      # iconTheme.name = "arc-icon-theme";
+      # theme.name = "art-theme";
+      iconTheme.name = "Adwaita";
+      theme.name = "Adwaita-dark";
+    };
+
+
+  };
+
+  # nixpkgs.overlays = [ waylandOverlay];
   nixpkgs.config.allowUnfree = true;
 
   # Use the systemd-boot EFI boot loader.
@@ -28,11 +184,10 @@ in
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  i18n = {
-    consoleFont = "latarcyrheb-sun32x24";
-    consoleKeyMap = "dvorak";
-    defaultLocale = "en_US.UTF-8";
-  };
+
+  console.font = "latarcyrheb-sun32x24";
+  console.keyMap = "dvorak";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
@@ -41,55 +196,53 @@ in
   # $ nix search wget
   #
 
-  programs.sway.enable = true;
-  programs.sway.extraSessionCommands = ''
-   ## Tell toolkits to use wayland
-   export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-   export CLUTTER_BACKEND=wayland
-   #export QT_QPA_PLATFORM=wayland-egl
-   export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-   export SQL_VIDEODRIVER=wayland
-   # Fix kirta and other egl using apps
-   export LD_LIBRARY_PATH=/run/opengl-driver/lib
+  # programs.sway.enable = true;
+  # programs.sway.extraSessionCommands = ''
+  #  ## Tell toolkits to use wayland
+  #  export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+  #  export CLUTTER_BACKEND=wayland
+  #  #export QT_QPA_PLATFORM=wayland-egl
+  #  export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+  #  export SQL_VIDEODRIVER=wayland
+  #  # Fix kirta and other egl using apps
+  #  export LD_LIBRARY_PATH=/run/opengl-driver/lib
+  #  WLR_DRM_NO_ATOMIC=1 sway
+  #  # for gameees
+  #  SDL_VIDEODRIVER=wayland
 
-  # Disable HiDPI scaling for X apps
-   export GDK_SCALE=1
-   export QT_AUTO_SCREEN_SCALE_FACTOR=0
-  '';
+  # # Disable HiDPI scaling for X apps
+  #  export GDK_SCALE=1
+  #  export QT_AUTO_SCREEN_SCALE_FACTOR=0
+  # '';
 
-  programs.sway.extraPackages = with pkgs; [
-	  xwayland
-	  swaybg
-	  swayidle
-	  swaylock
-	  waybar
-    #	i3status-rust
-	  gebaar-libinput
-	  grim
-	  kanshi
-	  # mako # notification deamon
-	  oguri
-	  redshift-wayland
-	  slurp
-	  wf-config
-	  wf-recorder
-	  wl-clipboard
-	  wtype
-    wofi
-  ];
+  # programs.sway.extraPackages = with pkgs; [
+	#   xwayland
+	#   swaybg
+	#   swayidle
+	#   swaylock
+	#   waybar
+  #   #	i3status-rust
+	#   gebaar-libinput
+	#   grim
+	#   kanshi
+	#   # mako # notification deamon
+	#   oguri
+	#   redshift-wayland
+	#   slurp
+	#   wf-recorder
+	#   wl-clipboard
+	#   wtype
+  #   wofi
+  # ];
 
+  # System packages accessable everywhere
   environment.systemPackages = with pkgs; [
 	  wget
     zsh
-    fzf
-    #autojump
-	
-    pmount
-    ripgrep
-    ripgrep-all
-    bat # like cat but with syntax
-    lf # like ranger
-    tldr # man examples
+	  vim
+	  firefox
+    git
+
 
     #FONTS
 	  source-code-pro
@@ -98,59 +251,6 @@ in
     liberation_ttf
     fira-code
     fira-code-symbols
-
-	  waybox
-	  # cage
-	  wayfire
-
-	  vim
-	  emacs
-	  firefox
-    chromium
-    htop
-    lollypop
-
-    alacritty #Terminal
-    rofi #X11 Launcher
-
-    #PROGRAMMING
-    git
-    nodejs
-    python
-    docker-compose
-    pandoc
-    tectonic
-    texlive.combined.scheme-full
-    ispell
-
-    filezilla
-    youtube-dl # youtube downloader
-    vlc
-    evince # pdfviewer
-    inkscape
-    libreoffice
-    gimp
-    krita
-    blender
-    tdesktop # Telegram desktop client
-    gnumake
-    #GTK SETTINGS
-    lxappearance
-    gtk_engines
-    glib
-    pango  # fonts stuff
-    arc-theme
-    arc-icon-theme
-    adementary-theme
-    gnome3.adwaita-icon-theme
-    adwaita-qt
-    # remove if they are not doing anything
-    atk
-    at-spi2-atk
-    xdg_utils
-
-    gnome3.nautilus #file manager
-    gnome3.eog
   ];
 
   fonts.fonts = with pkgs; [
@@ -201,8 +301,18 @@ in
   #       ];
   #   };
 
-  # programs.dconf.enable = true;
-  # services.dbus.packages = [ pkgs.gnome3.dconf ];
+ hardware.opengl = {
+    enable = true;
+    driSupport32Bit = true; # very important
+    driSupport = true;
+    extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  };
+
+
+
+
+ services.dbus.packages = with pkgs; [ gnome3.dconf gnome2.GConf ];
+ programs.dconf.enable = true; # for GNOME aps to accept themes
 
 
 
@@ -231,15 +341,17 @@ in
   hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
-  services.xserver.enable = false;
-  services.xserver.layout = "dvorak,us,sk";
-  services.xserver.xkbVariant = ",,qwerty";
-  services.xserver.xkbOptions = "eurosign:e, caps:escape";
+  # services.xserver.enable = true;
+  # services.xserver.layout = "dvorak,us,sk";
+  # services.xserver.xkbVariant = ",,qwerty";
+  # services.xserver.xkbOptions = "eurosign:e, caps:escape";
 
   # Enable touchpad support.
   # services.xserver.libinput.enable = true;
 
   # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
   # services.xserver.displayManager.lightdm.enable = true;
   # services.xserver.windowManager.i3.enable = true;
 
@@ -263,8 +375,8 @@ in
   };
 
   security.sudo.configFile = ''
-Defaults rootpw
-'';
+  Defaults rootpw
+  '';
 
 
   # Running unstable packages anyway !!
@@ -274,6 +386,6 @@ Defaults rootpw
   # servers. You should change this only after NixOS release notes say you
   # should.
   # system.autoUpgrade = true;
-  system.stateVersion = "19.09"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
 
 }
